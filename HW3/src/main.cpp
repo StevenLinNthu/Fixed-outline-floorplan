9744bb008df31d3744fc7eef5584324883a32149
StevenLinNthu <stevenlin0918@gmail.com>
2019-11-15 17:41:00 +0800

    add read file

--- HW3/src/main.cpp
+++ HW3/src/main.cpp
@@ -13,13 +13,11 @@
 #include<cstdlib>
 #include<cstring>
 #include<sys/time.h>
+#include<unordered_map>
 
 using namespace std;
 
-//global var
-int numBlock, numTerminal;
-int total_area;
-//
+
 struct terminal{
 	int id;
 	int x, y;
@@ -28,7 +26,7 @@ struct terminal{
 struct block{
 	int id;
 	int area;
-	int rotate;
+	bool rotate;
 	int w, h, x, y;
 	int parent, lc, rc;
 	//pointer?
@@ -36,9 +34,18 @@ struct block{
 
 struct net{
 	int deg;
+	vector<int> blockList, terminalList;
 	//ter list
 	//block list
 };
+
+//global var
+int numBlock, numTerminal, numNet, numPin;
+int total_area;
+unordered_map<int, block*> blocks;
+unordered_map<int, terminal*> terminals;
+unordered_map<int, net*> nets;
+//
 
 void read_block(FILE* input){
 	
@@ -49,14 +56,62 @@ void read_block(FILE* input){
 	
 		int a;
 		block* tmp = new block();
-		fscanf(input, " sb%d hardrectilinear 4 (%d, %d) (%d, %d) (%d, %d) (%d, %d)\n", &tmp->id, &a, &a, &a, &a, &tmp->w, &tmp->h, &a, &a);
+		fscanf(input, " sb%d hardrectilinear 4 (%d, %d) (%d, %d) (%d, %d) (%d, %d)\n", &tmp->id, &tmp->x, &tmp->y, &a, &a, &tmp->w, &tmp->h, &a, &a);
 	
 		tmp->area = tmp->w * tmp->h;
 		total_area += tmp->area;
+		tmp->rotate = false;
+		blocks[tmp->id] = tmp;
 	}
-
 	return;
 }
+
+void read_terminal(FILE* input){
+	int cnt = numTerminal;
+	while(cnt--){
+		terminal* tmp = new terminal();
+		fscanf(input, " p%d %d %d\n", &tmp->id, &tmp->x, &tmp->y);
+		terminals[tmp->id] = tmp;
+	}
+	return;
+}
+void read_net(FILE* input){
+	fscanf(input, " NumNets : %d\n", &numNet);
+	fscanf(input, " NumPins : %d\n", &numPin);
+	int cnt = numNet;
+	while(cnt--){
+		net* tmp = new net();
+		string str;
+		char c[30];
+		fscanf(input, " NetDegree : %d\n", &tmp->deg);		
+		for(int i=0; i<tmp->deg; i++){
+			fscanf(input, " %s\n", c);
+			str = c;
+			if(str[0] == 'p'){
+				str.erase(0,1);
+				tmp->terminalList.push_back(stoi(str));
+			}else{
+				str.erase(0,2);
+				tmp->blockList.push_back(stoi(str));
+			}
+		}
+	}
+	return;
+}
+
+int SA_floorplanning(int k){
+	E = init_sol();
+	Best = E;
+	float T0 = 10000;
+	int M = 0, MT = 0, uphill = 0;
+	int N = k*numBlock;
+	while(...){
+		
+	}
+	
+	
+	
+} 
 
 int main(void){
 	return 0;
